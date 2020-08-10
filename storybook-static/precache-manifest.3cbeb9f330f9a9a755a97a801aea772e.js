a1b98795af7116286aed775c2332a2dd55b41dbf
Tero <tero@starupcommons.org>
2020-08-10 22:18:06 +0300

    release

---
+++ storybook-static/precache-manifest.3cbeb9f330f9a9a755a97a801aea772e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d38b8ff103ff301b30ed9ba35f22d1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c63713ab90d350bdc5c0.bundle.js"
+  },
+  {
+    "url": "runtime~main.c63713ab90d350bdc5c0.bundle.js"
+  },
+  {
+    "url": "vendors~main.c63713ab90d350bdc5c0.bundle.js"
+  },
+  {
+    "url": "vendors~main.c63713ab90d350bdc5c0.bundle.js.LICENSE.txt"
+  }
+]);
